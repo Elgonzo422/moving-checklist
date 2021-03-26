a582d31847fd28126ea1eeb2147be8fd6633dadf
matt vile <matt.vile@yahoo.co.uk>
2021-03-26 10:22:33 +0000

    removed bad models

--- db/migrate/20210325163035_create_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateLists < ActiveRecord::Migration[6.1]
-  def change
-    create_table :lists do |t|
-      t.references :task, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
